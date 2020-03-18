ea21c8b39da4e2f92b70570a4dac1434d1f3ee5c
Hatemii <hatem.xhezairi96@gmail.com>
2020-03-18 23:09:54 +0100

    Working with handleChangeNames = (newName) =>{

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
 import React, { useState } from 'react';
 import Person from "./Person/Person"
 
-const App = props => {
-  const[personState,setPersonState]=useState({
+export default class App extends React.Component {
+  state = {
     persons:[
       {name:"Max",age:12},
       {name:"Larry",age:32},
       {name:"George",age:30}
     ],
     otherState: "some other value"
-  });
+  }
 
-  const [otherState,setOtherState] = useState({
-    otherState: "some other value 2"
-  });
-
-  console.log(personState,otherState);
-
-
-  const handleChangeNames = () =>{
-    setPersonState({
+ handleChangeNames = (newName) =>{
+    this.setState({
       persons:[
-        {name:"Maxim",age:12},
+        {name:newName,age:12},
         {name:"Laront",age:32},
         {name:"Paul",age:30}
       ]
     })
   }
 
-
+  render(){
     return(
       <div>
-        <button onClick={handleChangeNames}>Switch Names</button>
+        {/*this is first way of changing*/}
+        <button onClick={this.handleChangeNames.bind(this, "Mark")}>Switch Names</button>
 
-        <Person name={personState.persons[0].name} age={personState.persons[0].age} />
-        <Person name={personState.persons[1].name} age={personState.persons[1].age} />
-        <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+        {/*this is second way of changing*/}
+        <button onClick={() => this.handleChangeNames("Jordan")}>Switch Names2</button>
 
-
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     )
   }
-
-
-export default App;
+}
