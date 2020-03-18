5dfd24284e8d2ab956b297981d28e91e0894912b
Hatemii <hatem.xhezairi96@gmail.com>
2020-03-18 22:27:19 +0100

    Using State

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Person from "./Person/Person"
 
 
 class App extends React.Component{
+  state ={
+    persons:[
+      {name:"Max",age:12},
+      {name:"Larry",age:32},
+      {name:"George",age:30}
+    ]
+  }
+
   render(){
     return(
       <div>
-        <Person name="Lebron" age="34" />
-        <Person name="Alex" age="54" />
-        <Person name="John" age="24" >Hi im a children </Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
 
       </div>
     )
