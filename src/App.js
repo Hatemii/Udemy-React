d94dd7072d3051a05056aa20b595d4f0dbb57162
Hatemii <hatem.xhezairi96@gmail.com>
2020-03-18 22:40:09 +0100

    handleSwitchName (this.setState) with button

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ class App extends React.Component{
       {name:"Max",age:12},
       {name:"Larry",age:32},
       {name:"George",age:30}
-    ]
+    ],
+    otherState: "some other value"
+
+  }
+
+  handleChangeNames = () =>{
+    this.setState({
+      persons:[
+        {name:"Maxim",age:12},
+        {name:"Laront",age:32},
+        {name:"Paul",age:30}
+      ]
+    })
   }
 
   render(){
     return(
       <div>
+        <button onClick={this.handleChangeNames}>Switch Names</button>
+
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
