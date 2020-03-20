1823f7617b286fb590fbe92cfa776ee6fb4ac518
Hatemii <hatem.xhezairi96@gmail.com>
2020-03-20 21:55:12 +0100

    Show or Hide TOGGLE (this.state.showPerson ? ... : null

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default class Example extends React.Component {
       {name:"Larry",age:32},
       {name:"George",age:30}
     ],
-    otherState: "some other value"
+    otherState: "some other value",
+    showPersons: false
   }
 
  handleChangeNames = (newName) =>{
@@ -32,6 +33,15 @@ export default class Example extends React.Component {
     })
   }
 
+
+  handlePersonsToggle = () =>{
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
+
   render(){
     const style = {
       backgroundColor: "white",
@@ -41,20 +51,36 @@ export default class Example extends React.Component {
       cursor:"pointer"
     }
 
+
+
     return(
       <div className="App">
-        {/*this is first way of changing*/}
-        <button
-          style={style}
-          onClick={this.handleChangeNames.bind(this, "Mark")}>Switch Names</button>
+        {/*this is first way of changing values
+          <button style={style} onClick={this.handleChangeNames.bind(this, "Mark")}>Switch Names</button>*/}
 
-        {/*this is second way of changing
+        {/*this is second way of changing values
         <button onClick={() => this.handleChangeNames("Jordan")}>Switch Names2</button>*/}
 
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.handleOnChangeMethod} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    )
-  }
-}
+
+        {/* SHOW OR HIDE BY CLICKING*/}
+          {/*  /// this is important to know (dont forget {} brackets) ///
+          { this.state.showPerson ? .....
+          	now this showPerson is (false) and we want to chose what we want to do
+          	when showPerson goes true ....   : null }
+            : is like "else" in if else condition that we cant put something there or leave it null "nothing to do" }
+           */}
+
+           <button style={style} onClick={this.handlePersonsToggle}>Toggle</button>
+           {
+             this.state.showPersons === true ?
+             <div>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.handleOnChangeMethod} />
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+              </div>
+              : null
+              }
+          </div>
+          )
+        }
+      }
